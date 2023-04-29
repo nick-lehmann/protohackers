bc60e88768107d46d39e8a63ad1e76ad425999d0
Nick Lehmann <nick@lehmann.sh>
2023-04-29 09:36:00 +0000

    Add solution to problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = "0.0.0.0:60010";
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on: {}", addr);
+
+    loop {
+        // Accept new request
+        let (mut stream, _) = listener.accept().await?;
+        println!("New client connection");
+
+        // Handle new echo request
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = stream
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+                println!("Send back {:?} bytes to client", n);
+
+                if n == 0 {
+                    return;
+                }
+
+                stream
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to socket");
+            }
+        });
+    }
 }
